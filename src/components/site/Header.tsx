5c6db8fd78ed568fbeaabed3fa032a052ad22f83
kanishka bodaragama <bodaragamakanishka@gmail.com>
2026-07-06 18:34:29 +0530

    Update Header.tsx

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -58,7 +58,7 @@ type MobileMenuLogoControl = {
 const mobileMenuItemsControl: {
   xPx: number;
   yPx: number;
-  textAlignment: "center";
+  textAlignment: MobileMenuTextAlignment;
 } = {
   xPx: -50,
   yPx: 0,
